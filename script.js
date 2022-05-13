3a90500d3bf589d383015a04c4b0c1aad583d902
orangeola <orangeqmm@gmail.com>
2022-05-13 19:10:49 +0930

    Add slider that controls amount of squares in grid using a function.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+output.innerHTML = "Grid Size: " + slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = "Grid Size: " + this.value;
+  createGrid(this.value)
+} 
+
 const mainContainer = document.querySelector(".container");
-let gridSize = 25;
-mainContainer.style.gridTemplateColumns = "repeat(" + gridSize + ", 1fr)";
 
-for(let i = 0; i < gridSize*gridSize; i++)
+function createGrid(newSize)
 {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("block");
+    mainContainer.innerHTML = "";
+    mainContainer.style.gridTemplateColumns = "repeat(" + newSize + ", 1fr)";
 
-    newDiv.addEventListener("mouseenter", () => {
-        newDiv.style.backgroundColor = "black";
-    });
+    for(let i = 0; i < newSize*newSize; i++)
+        {
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("block");
 
-    mainContainer.appendChild(newDiv);
+            newDiv.addEventListener("mouseenter", () => {
+                newDiv.style.backgroundColor = "black";
+            });
+
+            mainContainer.appendChild(newDiv);
+        }
 }
+
+createGrid(25);
